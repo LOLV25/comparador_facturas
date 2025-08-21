a7e74c9d1073479a7cf5be64a9386cfd883dcfb3
Luis Olvera <luis-olv@outlook.es>
2025-08-21 16:52:15 -0500

    Agregué login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { FacturaComparador } from './components/factura-comparador/factura-comparador';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Login},
+    {path:'facturas',component:FacturaComparador}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { Navbar } from './components/navbar/navbar';
 import { FacturaComparador } from './components/factura-comparador/factura-comparador';
 import { Footer } from './components/footer/footer';
 import { DetalleFactura } from './components/detalle-factura/detalle-factura';
+import { Login } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, FacturaComparador,DetalleFactura, Footer],
+  //, Navbar, FacturaComparador, Footer
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
